4d0c0dabad1bdcf7221fb0efb6b4847741c5fd0f
Marc Magon <ged_marc@hotmail.com>
2017-10-16 21:21:12 +0200

    Updates and preparation for .12
    Signed-off-by: Marc Magon <ged_marc@hotmail.com>

--- test/za/co/mmagon/jwebswing/plugins/particlejs/ParticleJSPageConfiguratorTest.java
+++ test/za/co/mmagon/jwebswing/plugins/particlejs/ParticleJSPageConfiguratorTest.java
@@ -1,7 +1,7 @@
 package za.co.mmagon.jwebswing.plugins.particlejs;
 
 import org.junit.jupiter.api.Test;
-import za.co.mmagon.BaseTestClass;
+import za.co.mmagon.jwebswing.BaseTestClass;
 import za.co.mmagon.jwebswing.Page;
 
 /**
