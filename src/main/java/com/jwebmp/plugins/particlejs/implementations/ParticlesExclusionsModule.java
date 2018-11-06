41678cb65fba994437fb807df564383e7deddfcc
GedMarc <ged_marc@hotmail.com>
2018-11-06 21:29:24 +0200

    JRE 11 full split and module specifications a bit stricter

---
+++ src/main/java/com/jwebmp/plugins/particlejs/implementations/ParticlesExclusionsModule.java
@@ -0,0 +1,30 @@
+package com.jwebmp.plugins.particlejs.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ParticlesExclusionsModule
+		implements IGuiceScanModuleExclusions<ParticlesExclusionsModule>,
+				           IGuiceScanJarExclusions<ParticlesExclusionsModule>
+{
+
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-particlesjs-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.plugins.particlejs");
+		return strings;
+	}
+}
