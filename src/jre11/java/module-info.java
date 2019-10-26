fe0b59127f9479f548940a04f10dcaf1377c353f
GedMarc <ged_marc@hotmail.com>
2019-10-26 02:35:03 +0200

    Guiced EE Migration to its own space

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -5,18 +5,18 @@ module com.jwebmp.plugins.particlejs {
 	exports com.jwebmp.plugins.particlejs.themes;
 
 	requires com.jwebmp.core;
-	requires com.jwebmp.logmaster;
+	requires com.guicedee.logmaster;
 	requires com.fasterxml.jackson.annotation;
 
 	requires java.validation;
 	requires java.logging;
 
-	requires com.jwebmp.guicedinjection;
+	requires com.guicedee.guicedinjection;
 	requires com.fasterxml.jackson.databind;
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.particlejs.ParticleJSPageConfigurator;
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.particlejs.implementations.ParticlesExclusionsModule;
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.particlejs.implementations.ParticlesExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.particlejs.implementations.ParticlesExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.particlejs.implementations.ParticlesExclusionsModule;
 
 	opens com.jwebmp.plugins.particlejs to com.fasterxml.jackson.databind, com.jwebmp.core;
 	opens com.jwebmp.plugins.particlejs.options to com.fasterxml.jackson.databind, com.jwebmp.core;
--- src/main/java/com/jwebmp/plugins/particlejs/themes/ParticleThemes.java
+++ src/main/java/com/jwebmp/plugins/particlejs/themes/ParticleThemes.java
@@ -19,7 +19,7 @@ package com.jwebmp.plugins.particlejs.themes;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jwebmp.core.FileTemplates;
-import com.jwebmp.guicedinjection.GuiceContext;
+import com.guicedee.guicedinjection.GuiceContext;
 import com.jwebmp.plugins.particlejs.ParticlesJSOptions;
 
 import javax.validation.constraints.NotNull;
