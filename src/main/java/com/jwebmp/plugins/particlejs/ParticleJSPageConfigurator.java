f3d128c3b580e77cf4bad6dfc30f3ffdce0058ef
GedMarc <ged_marc@hotmail.com>
2020-12-17 09:02:27 +0200

    Pge configurator type safety and test class updates for it

--- src/main/java/com/jwebmp/plugins/particlejs/ParticleJSPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/particlejs/ParticleJSPageConfigurator.java
@@ -96,7 +96,7 @@ public class ParticleJSPageConfigurator
 
 	@NotNull
 	@Override
-	public Page configure(Page page)
+	 public Page<?> configure(Page<?> page)
 	{
 		if (!page.isConfigured() && enabled())
 		{
